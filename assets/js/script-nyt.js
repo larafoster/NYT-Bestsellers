55bfaeba3c921a2f75c17806f094dd499f675ce7
Zach Young <Zyoung.young9@gmail.com>
2021-07-22 10:19:22 -0400

    update

--- assets/js/script-nyt.js
+++ assets/js/script-nyt.js
@@ -11,13 +11,10 @@ var apiKey = 'Xnbc5raSBOZ8T953UYXEizN4qTN8wBDX';
 fetch('https://api.nytimes.com/svc/books/v3/lists.json?list-name=hardcover-fiction&api-key=' + apiKey, {method:'get',})
 .then(response => { return response.json(); }) 
 .then(json => { console.log(json); 
-    });
 
-
-
-// nytimesBestSellers.results.forEach(function(book) {
-//     var isbn = book.isbns[1].isbn10;  var bookInfo = book.book_details[0];
-//     });
+     json.results.forEach(function(book) {
+     var isbn = book.isbns[1].isbn10;  var bookInfo = book.book_details[0];
+     });});
 
     
   $(document).ready(function(){
